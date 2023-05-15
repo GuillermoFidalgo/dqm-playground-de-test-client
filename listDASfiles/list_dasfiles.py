448e2aed58dc1264791cfc297aa3d29bc0eb6940
Guillermo Fidalgo <guillermo.fidalgo@upr.edu>
2023-05-15 20:27:00 +0200

    updating scripts

--- listDASfiles/list_dasfiles.py
+++ listDASfiles/list_dasfiles.py
@@ -1,13 +1,34 @@
 # IMPORTANT This file does not run form inside SWAN env. Only run this on a terminal and then cd into the working directory.
 
-import subprocess
+import subprocess,os, argparse
+
+
+
+# read arguments
+parser = argparse.ArgumentParser(description='Make a list of files found on DAS')
+
+parser.add_argument('--outputdir', default=os.path.abspath('.'),
+                  help='Local directory where to put the copied file')
+
+
+parser.add_argument("--listfiles",type=str,
+                    default='listfiles.txt',
+                    help='A text file with list of paths from DAS.')
+
+args = parser.parse_args()
+
+
+outputdir = args.outputdir
+
+listfiles = args.listfiles
+
 
 eras = ['A','B','C','D','E','F','G']
 for i in eras:
     
     if i == 'A':
-        cmd = f'dasgoclient -query="file dataset=/ZeroBias/Run2022{i}-19Jan2023-v2/DQMIO" | grep "/store" > listfiles.txt' 
+        cmd = f'dasgoclient -query="file dataset=/ZeroBias/Run2022{i}-19Jan2023-v2/DQMIO" | grep "/store" > {outputdir}/{listfiles}' 
     else:
-        cmd = f'dasgoclient -query="file dataset=/ZeroBias/Run2022{i}-19Jan2023-v2/DQMIO" | grep "/store" >> listfiles.txt'
+        cmd = f'dasgoclient -query="file dataset=/ZeroBias/Run2022{i}-19Jan2023-v2/DQMIO" | grep "/store" >> {outputdir}/{listfiles}'
     
     subprocess.run(cmd, shell=True, check=False)
